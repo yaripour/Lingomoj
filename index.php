e3f41ad69bebd8102b2fe264e185cf982749a031
yaripour <yaripour.m@gmail.com>
2021-08-11 13:53:53 +0430

    added all files

---
+++ index.php
@@ -0,0 +1,457 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>First Project</title>
+</head>
+<body>
+<div class="top-menu">
+    <div class="container">
+        <div class="topbar-right">
+            <ul>
+                <li><i class="fas fa-phone"></i>مشاوره و پشتیبانی واتس اپ: 09383194723</li>
+                <li><i class="fas fa-envelope"></i>ایمیل: info@siteskin.ir</li>
+
+            </ul>
+
+
+        </div>
+        <div class="topbar-left">
+            <ul>
+                <li><i class="fas fa-search"></i></li>
+                <li><i class="fas fa-shopping-basket"></i></li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+
+</div>
+<header class="header">
+    <div class="container">
+        <div class="logo">
+            <img src="img/logo.jpg">
+        </div>
+        <nav class="main-menu">
+            <ul>
+                <li><a href="#">آموزش انگلیسی<i class="fas fa-angle-down"> </i></a>
+                    <ul>
+                        <li><a href="#">کودکان</a></li>
+                        <li><a href="#">نوجوانان</a></li>
+                        <li><a href="#">بزرگسالان</a></li>
+                        <li><a href="#">داستان</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="#">آموزش آلمانی</a></li>
+                <li><a href="#">آموزش فرانسه<i class="fas fa-angle-down"> </i></a>
+                    <ul>
+                        <li><a href="#">مبتدی</a></li>
+                        <li><a href="#">متوسطه</a></li>
+                        <li><a href="#">پیشرفته</a></li>
+                        <li><a href="#">داستان</a></li>
+
+                    </ul>
+
+                </li>
+            </ul>
+        </nav>
+        <div class="sign">
+            <a href="#"><i class="fas fa-user-lock"></i>ورود/ثبت نام </a>
+
+        </div>
+
+    </div>
+
+
+</header>
+<section class="box-slider">
+    <div id="main-slider" class="owl-carousel owl-theme">
+        <div class="item"><img src="img/1.jpg"></div>
+        <div class="item"><img src="img/2.jpg"></div>
+    </div>
+</section>
+
+
+<section class="tv">
+    <div class="container">
+        <div class="tv-head">
+            <div class="tv-titile">
+                <h2>مای لینگو تیم</h2>
+                <h5>ویدیوهای آموزشی</h5>
+
+            </div>
+            <div class="tv-link">
+                <a href="#">همه ویدیوها</a>
+            </div>
+        </div>
+        <div class="video-box">
+            <div class="right-videobox">
+                <div class="main-video">
+
+                    <a href="#">
+                        <figure>
+                            <img src="img/3.jpg" >
+                            <i class="fas fa-play-circle"></i>
+                        </figure>
+                    </a>
+
+                </div>
+
+
+            </div>
+            <div class="left-videobox">
+                <div class="other-videos">
+
+                    <a href="#">
+                        <figure>
+                            <img src="img/other-videos1.jpg" >
+                            <i class="fas fa-play"></i>
+                            <h2>آموزش انگلیسی برای کودکان سه تا پنج سال</h2>
+                        </figure>
+                    </a>
+
+                </div>
+                <div class="other-videos">
+
+                    <a href="#">
+                        <figure>
+                            <img src="img/other-videos2.jpg" >
+                            <i class="fas fa-play"></i>
+                            <h2>آموزش انگلیسی برای کودکان سه تا پنج سال</h2>
+                        </figure>
+                    </a>
+
+                </div>
+                <div class="other-videos">
+
+                    <a href="#">
+                        <figure>
+                            <img src="img/other-videos3.jpg" >
+                            <i class="fas fa-play"></i>
+                            <h2>آموزش انگلیسی برای کودکان سه تا پنج سال</h2>
+                        </figure>
+                    </a>
+
+                </div>
+                <div class="other-videos">
+
+                    <a href="#">
+                        <figure>
+                            <img src="img/other-videos4.jpg" >
+                            <i class="fas fa-play"></i>
+                            <h2>آموزش انگلیسی برای کودکان سه تا پنج سال</h2>
+                        </figure>
+                    </a>
+
+                </div>
+                <div class="all-videos">
+                   <a href="#">تماشای همه ویدیوها</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+    <div class="line"></div>
+<div class="line"></div>
+
+
+<section class="articles">
+    <div class="container">
+        <div class="articles-head">
+            <div class="articles-titile">
+                <h2>آخرین مقالات</h2>
+                <h5>بلاگ</h5>
+
+            </div>
+            <div class="articles-link">
+                <a href="#">همه مقالات</a>
+            </div>
+        </div>
+        <div class="articles-slider">
+                <div id="articles-slider" class="owl-carousel owl-theme">
+
+                    <div class="item article-box">
+                            <a href="#"><img src="img/article1.jpg">
+                            <h2>روش های خلاقانه تدریس زبان انگلیسی به کودکان</h2></a>
+                            <p>کودکان کلاس خود را به چالش بکشید تا مسئولیت یادگیری خود را بر عهده بگیرند</p>
+                            <div class="btn-more">
+                                <a href="#">ادامه مطلب</a>
+                            </div>
+                    </div>
+
+                    <div class="item article-box">
+                        <a href="#"><img src="img/article2.jpg">
+                            <h2>سه ترفند قدرتمند برای گرفتن بهترین نتیجه</h2></a>
+                        <p>تفریح٬ سرگرمی و شادی فاکتورهای بسیار مهم در آموزش به کودکان می باشد</p>
+                        <div class="btn-more">
+                            <a href="#">ادامه مطلب</a>
+                        </div>
+                    </div>
+                    <div class="item article-box">
+                        <a href="#"><img src="img/article3.jpg">
+                            <h2>بازی های مناسب برای آموزش زبان به کودکان</h2></a>
+                        <p>اگر یادگیری با سرگرمی و بازی همراه ‌شود کودکان نیز آن را جالب میدانند</p>
+                        <div class="btn-more">
+                            <a href="#">ادامه مطلب</a>
+                        </div>
+                    </div>
+                    <div class="item article-box">
+                        <a href="#"><img src="img/article4.jpg">
+                            <h2>افزایش علاقه به یادگیری انگلیسی در کودکان</h2></a>
+                        <p>آیا میدانستید کودکان در تعامل با والدین خود زبان را بهتر یاد میگیرند</p>
+                        <div class="btn-more">
+                            <a href="#">ادامه مطلب</a>
+                        </div>
+                    </div>
+                    <div class="item article-box">
+                        <a href="#"><img src="img/article5.jpg">
+                            <h2>افزایش علاقه به یادگیری انگلیسی در کودکان</h2></a>
+                        <p>آیا میدانستید کودکان در تعامل با والدین خود زبان را بهتر یاد میگیرند</p>
+                        <div class="btn-more">
+                            <a href="#">ادامه مطلب</a>
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="adv">
+    <div class="container">
+        <div class="right-adv">
+            <h2 class="adv-title">تعیین سطح رایگان</h2>
+            <p class="adv-desc">با انجام این آزمون تعیین سطح آنلاین کاملا رایگان، از سطح تقریبی زبان خود آگاه خواهید شد.<br>برای شروع روی دکمه زیر کلیک کنید</p>
+            <a href="#">شروع تعیین سطح</a>
+        </div>
+        <div class="left-adv">
+            <figure>
+                <img src="img/1.webp">
+            </figure>
+
+        </div>
+    </div>
+
+</section>
+
+
+
+
+<section class="course">
+    <div class="container">
+        <div class="course-head">
+            <div class="course-titile">
+                <h2>دوره های ویژه</h2>
+                <h5>دوره ها</h5>
+
+            </div>
+            <div class="course-link">
+                <a href="#">آرشیو دوره ها</a>
+            </div>
+        </div>
+        <div class="course-slider">
+            <div id="course-slider" class="owl-carousel owl-theme">
+
+                <div class="item course-box">
+                    <a href="#"><img src="img/course1.jpg">
+                        <h2>آموزش گرامر انگلیسی به زبان ساده</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,000,000</del>
+                            <span>رایگان</span>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>18
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="img/course2.jpg">
+                        <h2>دوره آموزشی انگلیسی در سفر</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>730,000</del>
+                            <ins>580,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>28
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="img/course3.jpg">
+                        <h2>دوره مکالمه انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                            <ins>1,280,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>22
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="img/course4.jpg">
+                        <h2>دوره ی خود آموز انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                           <span>رایگان</span>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>48
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="img/course5.jpg">
+                        <h2>لغات کاربری انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                            <ins>1,280,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>18
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</section>
+
+<footer>
+    <div class="container">
+        <div class="about-us">
+            <h3>درباره ما</h3>
+            <p>گروه آموزشی زبان های خارجی my lingo team بر آن شد تا به یاری زبان آموزان عزیز آمده و با تشکیل گروهی حرفه ای از معلمان با سابقه و کلاسهایی با جدیدترین متد های آموزشی و همچنین فراهم کردن منابع آموزشی متعدد مسیر یادگیری را برای شما سهولت می بخشد . معلم زبان و کلاس مناسب خودتان را از گروه my lingo team انتخاب کنید و یادگیری لذت بخشی را شروع کنید .</p>
+        </div>
+        <div id="qa" class="quick-access">
+            <h3>دسترسی سریع</h3>
+            <ul>
+                <li><a href="#">معرفی مدرسین</a></li>
+                <li><a href="#">سوالات متداول</a></li>
+                <li><a href="#">قوانین و مقررات</a></li>
+                <li><a href="#">وبلاگ آموزشی</a></li>
+            </ul>
+        </div>
+        <div id="contact" class="quick-access">
+            <h3>تماس با ما</h3>
+            <ul>
+                <li><i class="fas fa-inbox"></i> <span>ایمیل: info@mylingoteam.com</span></li>
+                <li><i class="fas fa-mobile"></i><span>موبایل: 09383194723</span></li>
+               <li><i class="fas fa-home"></i><span>آدرس: رشت - بلوار انصاری</span></li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="copy-right">
+        <p>تمامی حقوق سایت برای مای لینگوتیم محفوظ است و کپی برداری تنها با ذکر منبع آزاد است </p>
+        <div class="social-media">
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+    </div>
+</footer>
+
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,71 @@
+$('#main-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    autoplay:true,
+})
+$('#articles-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    },
+    autoplay:true,
+})
+$('#course-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    },
+
+    autoplay:false,
+})
+
+$('#related-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    },
+
+    autoplay:false,
+})
