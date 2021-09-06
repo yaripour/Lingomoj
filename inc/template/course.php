4b5ec7b8881015f0446c90c2af008bb2ae880b93
yaripour <yaripour.m@gmail.com>
2021-09-06 12:45:43 +0430

    split course part and createt course.php

---
+++ inc/template/course.php
@@ -0,0 +1,162 @@
+<section class="course">
+    <div class="container">
+        <div class="course-head">
+            <div class="course-titile">
+                <h2>دوره های ویژه</h2>
+                <h5>دوره ها</h5>
+
+            </div>
+            <div class="course-link">
+                <a href="#">آرشیو دوره ها</a>
+            </div>
+        </div>
+        <div class="course-slider">
+            <div id="course-slider" class="owl-carousel owl-theme">
+
+                <div class="item course-box">
+                    <a href="#"><img src="<?php echo get_template_directory_uri(). '/img/course1.jpg'?>">
+                        <h2>آموزش گرامر انگلیسی به زبان ساده</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,000,000</del>
+                            <span>رایگان</span>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>18
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="<?php echo get_template_directory_uri(). '/img/course2.jpg'?>">
+                        <h2>دوره آموزشی انگلیسی در سفر</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>730,000</del>
+                            <ins>580,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>28
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="<?php echo get_template_directory_uri(). '/img/course3.jpg'?>">
+                        <h2>دوره مکالمه انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                            <ins>1,280,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>22
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="<?php echo get_template_directory_uri(). '/img/course4.jpg'?>">
+                        <h2>دوره ی خود آموز انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                            <span>رایگان</span>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>48
+                        </div>
+                    </div>
+                </div>
+                <div class="item course-box">
+                    <a href="#"><img src="<?php echo get_template_directory_uri(). '/img/course5.jpg'?>">
+                        <h2>لغات کاربری انگلیسی</h2></a>
+                    <div class="description">
+
+                        <div class="teacher">
+                            <span>یاری پور</span>
+                            <i class="fas fa-chalkboard-teacher"></i>
+                        </div>
+                        <div class="rate">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+
+                        </div>
+                    </div>
+                    <div class="details">
+                        <div class="price">
+                            <del>1,650,000</del>
+                            <ins>1,280,000</ins>
+                        </div>
+                        <div class="users">
+                            <i class="fas fa-users"></i>18
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</section>
