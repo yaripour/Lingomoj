5fb7a47fb14e6917b42f793c63336daa44b9beed
yaripour <yaripour.m@gmail.com>
2021-10-02 23:41:50 +0330

    sabade kharid

---
+++ admin/content-tab-lesson.php
@@ -0,0 +1,44 @@
+
+<?php
+
+$course = get_post_meta(get_the_ID() , 'lingomoj_group_lesson' , true);
+
+
+if (!empty($course)) {
+    ?>
+    <div class="lesson-course">
+        <ul>
+            <?php foreach ( $course as $item) : ?>
+                <li>
+                    <h4><?php echo $item['lingomoj_course_chapter_title']; ?>
+                        <i class="fas fa-angle-down"></i>
+                    </h4>
+                    <ul>
+                        <div class="meta-course">
+                            <div class="time-course">
+                                <i class="fas fa-clock"></i>
+                                <span>مدت زمان فصل : </span>
+                                <span><?php echo $item['lingomoj_course_chapter_time']; ?></span>
+                            </div>
+                            <div class="dl-course">
+                                <a href="<?php echo $item['lingomoj_course_chapter_link']; ?>" target="_blank">
+                                    <i class="fas fa-download"></i>
+                                    لینک دانلود فصل
+                                </a>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+
+                        <?php foreach ($item['lingomoj_course_chapter_lesson'] as $value) : ?>
+                            <li><i class="fas fa-check"></i>
+                                <?php echo $value; ?>
+                            </li>
+                        <?php endforeach; ?>
+
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+
+        </ul>
+    </div>
+<?php } ?>
--- woocommerce.php
+++ woocommerce.php
@@ -1,50 +1,83 @@
-<?php get_header() ?>
-    <div class="single">
+<?php get_header(); ?>
+
+    <div class="single-page">
         <div class="container">
             <div class="main-single main-pro">
+
                 <div class="product-title">
                     <header>
                         <h1><?php the_title(); ?></h1>
                     </header>
                 </div>
-                <div class="product-thumbnail"
-                <div class="post-pic">
+
+                <div class="product-thumbnail">
                     <figure>
                         <?php
-                        $video = get_post_meta(get_the_ID(), key: 'mylingo_video_product', single: true);
-                        $poster= get_post_meta(get_the_ID(), key: 'mylingo_video_product_poster', single: true);
-                        if(!empty($video)){
+                        $video_pro = get_post_meta(get_the_ID() , 'lingomoj_video_product' , true);
+                        $poster = get_post_meta(get_the_ID() , 'lingomoj_video_product_poster' , true);
+                        if (!empty($video_pro)) {
                             $attr = array(
-                                'src' => $video,
-                                'width' => '800',
-                                'height' => '400',
-                                'poster' =>'$poster',
-
+                                'src'   =>  $video_pro,
+                                'width' => '1200',
+                                'height'    => '700',
+                                'poster' => $poster,
                             );
                             echo wp_video_shortcode($attr);
-                        } else {
+                        }
+                        else {
                             the_post_thumbnail();
                         }
                         ?>
                     </figure>
                 </div>
 
-                <article class="single-product woocommerce">
-                    <div class="content-single"
-                    <?php woocommerce_content(); ?>
+                <article class="product-single woocommerce">
+                    <div class="content-single">
+                        <?php woocommerce_content(); ?>
                     </div>
                 </article>
 
-            <div class="related-product woocommerce">
-                <?php echo do_shortcode('[related_products per_page=3 columns=3]'); ?>
+                <div class="related-product woocommerce">
+                    <?php echo do_shortcode('[related_products per_page=3 columns=3]'); ?>
+                </div>
             </div>
 
+            <div class="sidebar side-pro">
+                <div class="single-widget">
+                    <div class="pro-price">
+                        <span class="price-name"> قیمت : </span>
+                        <p class="<?php global $product; echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
+                    </div>
+                    <?php
+                    $id = $product->id;
+                    if (is_user_logged_in()) {
+                        $current_user = wp_get_current_user();
+                    }
+                    if (wc_customer_bought_product(  $current_user->user_email ,  $current_user->ID , $id )){
+                        ?>
+                        <div class="price-button">
+                            <a>
+                                <i class="fas fa-credit-card"></i>
+                                دانشجوی دوره هستید!
+                            </a>
+                        </div>
+                        <?php
+                    }
+                    else {
+                        ?>
+                        <div class="price-button">
+                            <a href="<?php echo do_shortcode("[add_to_cart_url id=$id]"); ?>">
+                                <i class="fas fa-credit-card"></i>
+                                ثبت نام دوره
+                            </a>
+                        </div>
+                        <?php
+                    }
+                    ?>
 
-
-        </div>
-
+                </div>
+            </div>
         </div>
     </div>
 
-
-<?php get_footer() ?>
+<?php get_footer(); ?>
